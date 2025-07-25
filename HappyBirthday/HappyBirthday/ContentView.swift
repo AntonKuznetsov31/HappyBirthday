4212b9bcf026ab66232b8a4d6e699069f4cad355
Anton Kuznetsov <anton.kuznetsov.mail@gmail.com>
2025-07-26 00:01:32 +0200

    Project created
    git ignore file corrections
    initial project files

---
+++ HappyBirthday/HappyBirthday/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  HappyBirthday
+//
+//  Created by Anton Kuznetsov on 25.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HappyBirthday/HappyBirthday/HappyBirthdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBirthdayApp.swift
+//  HappyBirthday
+//
+//  Created by Anton Kuznetsov on 25.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct HappyBirthdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
